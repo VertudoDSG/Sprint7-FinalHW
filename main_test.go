080294a869347f2cba8b572801d6e32b7ad54ba4
Полина Грунина <linaborisovsky@yandex.ru>
2025-12-10 18:59:15 +0300

    first commit

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"strings"
 	"testing"
 
@@ -46,5 +47,72 @@ func TestCafeWhenOk(t *testing.T) {
 		handler.ServeHTTP(response, req)
 
 		assert.Equal(t, http.StatusOK, response.Code)
+	}
+}
+
+func TestCafeCount(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+
+	requests := []struct {
+		count int
+		want  int
+	}{
+		{count: 0, want: 0},
+		{count: 1, want: 1},
+		{count: 2, want: 2},
+		{count: 100, want: min(len(cafeList["moscow"]), 100)},
+	}
+
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", "/cafe?city=moscow&count="+strconv.Itoa(v.count), nil)
+
+		handler.ServeHTTP(response, req)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+
+		body := strings.TrimSpace(response.Body.String())
+		if body == "" {
+			assert.Equal(t, 0, v.want)
+			continue
+		}
+
+		got := len(strings.Split(body, ","))
+		assert.Equal(t, v.want, got)
+	}
+}
+
+func TestCafeSearch(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+
+	requests := []struct {
+		search    string
+		wantCount int
+	}{
+		{search: "фасоль", wantCount: 0},
+		{search: "кофе", wantCount: 2},
+		{search: "вилка", wantCount: 1},
+	}
+
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", "/cafe?city=moscow&search="+v.search, nil)
+
+		handler.ServeHTTP(response, req)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+
+		body := strings.TrimSpace(response.Body.String())
+		if body == "" {
+			assert.Equal(t, 0, v.wantCount)
+			continue
+		}
+
+		cafes := strings.Split(body, ",")
+		assert.Equal(t, v.wantCount, len(cafes))
+
+		for _, cafe := range cafes {
+			assert.True(t, strings.Contains(strings.ToLower(cafe), strings.ToLower(v.search)))
+		}
 	}
 }
